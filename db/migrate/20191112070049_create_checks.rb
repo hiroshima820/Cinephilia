b0c5f8ff7d8d32d5dc8a2cedf181d9f61c28e2aa
hiroshima820 <hiroshima820@gmail.com>
2019-11-28 09:24:45 +0000

    Fixed migration file.s

--- db/migrate/20191112070049_create_checks.rb
+++ db/migrate/20191112070049_create_checks.rb
@@ -3,7 +3,7 @@ class CreateChecks < ActiveRecord::Migration[5.2]
     create_table :checks do |t|
       t.integer :user_id
       t.integer :tmdb_movie_id
-      t.boolean :prepare
+      t.boolean :prepare, default: false, null: false
 
       t.timestamps
     end
